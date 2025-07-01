06a18223dc4e495585232dafd41adeb2a15bf05f
George Rawlinson <TheGogy@proton.me>
2025-07-01 23:41:08 +0100

    try with 1 min reduction but no offset | bench 2377087

--- nnue/src/arch/flatten.rs
+++ nnue/src/arch/flatten.rs
@@ -24,7 +24,6 @@ mod fallback {
     /// Flatten the accumulator using the given weights. (fallback: non-vectorized)
     pub fn flatten(acc: &SideAccumulator, weights: &SideAccumulator) -> i32 {
         let mut sum = 0;
-        println!("FALLBACK");
 
         for (&x, &w) in acc.0.iter().zip(&weights.0) {
             sum += screlu(x) * w as i32;
