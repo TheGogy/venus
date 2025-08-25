0a4a52c5a9612837a608c393e89a70b782c99b35
TheGogy <TheGogy@proton.me>
2025-08-25 22:32:00 +0100

    Output bucket nnue architecture
    Seems to be an improvement already, not even at final ver
    bench 2266505

--- nnue/src/arch/mod.rs
+++ nnue/src/arch/mod.rs
@@ -14,29 +14,30 @@ pub const SCALE: i32 = 400;
 // Layer sizes.
 // These should ideally be divisible by Simd::CHUNK_SIZE.
 pub const FEATURES: usize = 768;
-pub const L1: usize = 1536;
+pub const L1: usize = 2048;
 
 // King bucket map (horizontally mirrored).
 #[rustfmt::skip]
 const BUCKET_MAP: [usize; 64] = [
-    0,  0,  1,  1,  9,  9,  8,  8,
-    2,  2,  3,  3, 11, 11, 10, 10,
-    4,  4,  5,  5, 13, 13, 12, 12,
-    6,  6,  7,  7, 15, 15, 14, 14,
-    6,  6,  7,  7, 15, 15, 14, 14,
-    6,  6,  7,  7, 15, 15, 14, 14,
-    6,  6,  7,  7, 15, 15, 14, 14,
-    6,  6,  7,  7, 15, 15, 14, 14,
+    0,  1,  2,  3, 13, 12, 11, 10,
+    4,  4,  5,  5, 15, 15, 14, 14,
+    6,  6,  6,  6, 16, 16, 16, 16,
+    7,  7,  7,  7, 17, 17, 17, 17,
+    8,  8,  8,  8, 18, 18, 18, 18,
+    8,  8,  8,  8, 18, 18, 18, 18,
+    9,  9,  9,  9, 19, 19, 19, 19,
+    9,  9,  9,  9, 19, 19, 19, 19,
 ];
 
-pub const BUCKET_NUM: usize = 8;
+pub const NB_INPUT_BUCKETS: usize = 10;
+pub const NB_OUTPUT_BUCKETS: usize = 8;
 
 /// Weights and biases for the NNUE.
 #[repr(C)]
 #[rustfmt::skip]
 pub struct NNUEData {
-    pub feature_weights: Align64<[i16; L1 * FEATURES * BUCKET_NUM]>,
-    pub feature_bias:    Align64<[i16; L1]>,
-    pub output_weights: [Align64<[i16; L1]>; 2],
-    pub output_bias: i16,
+    pub feature_weights:  Align64<[i16; L1 * FEATURES * NB_INPUT_BUCKETS]>,
+    pub feature_bias:     Align64<[i16; L1]>,
+    pub output_weights: [[Align64<[i16; L1]>; 2]; NB_OUTPUT_BUCKETS],
+    pub output_bias:      Align64<[i16; NB_OUTPUT_BUCKETS]>,
 }
--- nnue/src/arch/utils.rs
+++ nnue/src/arch/utils.rs
@@ -1,5 +1,7 @@
 use chess::types::{color::Color, square::Square};
 
+use crate::arch::NB_OUTPUT_BUCKETS;
+
 use super::{BUCKET_MAP, L1};
 
 /// Get the index of the features representing a given color and piece for white and black.
@@ -17,8 +19,8 @@ pub const fn ft_idx(c: usize, p: usize, mut wksq: Square, mut bksq: Square, s: S
         bflip ^= 7
     }
 
-    let wbucket = bucket_idx(wksq, Color::White);
-    let bbucket = bucket_idx(bksq, Color::Black);
+    let wbucket = input_bucket_idx(wksq, Color::White);
+    let bbucket = input_bucket_idx(bksq, Color::Black);
     let s = s.idx();
 
     let w = wbucket * 768 +      c  * 384 + p * 64 + (s ^ wflip);
@@ -28,6 +30,12 @@ pub const fn ft_idx(c: usize, p: usize, mut wksq: Square, mut bksq: Square, s: S
 }
 
 /// Get the current input bucket to use.
-pub const fn bucket_idx(ksq: Square, c: Color) -> usize {
+pub const fn input_bucket_idx(ksq: Square, c: Color) -> usize {
     BUCKET_MAP[ksq.idx() ^ (56 * c.idx())]
+}
+
+/// Get the current output bucket to use.
+pub const fn output_bucket_idx(nb_pieces: usize) -> usize {
+    const DIV: usize = usize::div_ceil(32, NB_OUTPUT_BUCKETS);
+    (nb_pieces - 2) / DIV
 }
